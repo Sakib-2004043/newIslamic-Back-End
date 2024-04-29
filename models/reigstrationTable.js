6fa27d071dc9072d516a99898ea42b1fae16869d
Sakib-2004043 <sakib1514817122@gmail.com>
2024-04-30 00:54:55 +0600

    last

--- models/reigstrationTable.js
+++ models/reigstrationTable.js
@@ -15,7 +15,7 @@ const registrationSchema = mongoose.Schema({
   password : {
     type: String,
     require : true
-  }  
+  }
 })
 
 module.exports = mongoose.model("registrationTables",registrationSchema);
